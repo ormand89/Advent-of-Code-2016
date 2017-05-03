8d31822e1477ffacb68afeec75e9b228d31e2893
Ormand89 <komoskoav@gmail.com>
2017-05-03 19:18:27 +0300

    Remove unused

--- README.md
+++
@@ -1,3 +0,0 @@
-# Advent-of-Code-2016
-First try
-First time
--- sides.rb
+++
@@ -1,8 +0,0 @@
-class Sides
-  attr_accessor :coordinates
-  def turn_left(current_position, x)
-  end
-
-  def turn_right(current_position, x)
-  end
-end
