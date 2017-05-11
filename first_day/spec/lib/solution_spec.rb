6a9baa309afde808725f3612ef12ae63f88067c1
Ormand89 <komoskoav@gmail.com>
2017-05-11 12:01:36 +0300

    Update solution_spec

--- first_day/spec/lib/solution_spec.rb
+++ first_day/spec/lib/solution_spec.rb
@@ -3,20 +3,23 @@ require 'solution'
 
 RSpec.describe Solution do
   describe '#go' do
-    let(:input) { File.expand_path('../fixtures/task', File.dirname(__FILE__)) }
-    let(:expected_output) { 4 }
-    subject(:code) { described_class.new(input) }
+    shared_examples 'check solution order' do |input:, expected_output:|
+      context "for #{input}" do
+        subject(:code) { described_class.new(input) }
 
-    it 'returns steps from start for input steps' do
-      expect(subject.go).to eq(expected_output)
+        it 'returns steps from start for input file' do
+          expect(subject.go).to eq(expected_output)
+        end
+      end
     end
-#To solve the advantage
-    let(:input) { File.expand_path('../../lib/task', File.dirname(__FILE__)) }
-    let(:expected_output) { 299 }
-    subject(:code) { described_class.new(input) }
 
-    it 'returns steps from start to solve advantage' do
-      expect(subject.go).to eq(expected_output)
-    end
+      include_examples 'check solution order', input: File.expand_path('../fixtures/task', File.dirname(__FILE__)),
+                                               expected_output: 4
+      include_examples 'check solution order', input: File.expand_path('../../lib/task', File.dirname(__FILE__)),
+                                               expected_output: 299 #To solve the advantage
+
   end
 end
+
+
+
